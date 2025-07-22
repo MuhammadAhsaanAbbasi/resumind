bcb738bfa8b7bd057eb015b960c7a633669c36a8
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-07-22 07:34:02 +0500

    Landing Page Completed

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <Link to='/'>
+            <h1 className='font-bold text-2xl text-gradient'>
+                Resumind
+            </h1>
+        </Link>
+        <Link to='/upload' className='primary-button w-fit'>
+            Upload Resume
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "~/../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
@@ -10,10 +13,30 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Resumind
-      </h1>
-    </div>
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-12">
+          <h1>
+            Track Your Applications & Resume Ratings
+          </h1>
+          <h2>
+            Review your submissions and check AI-powered feedback.
+          </h2>
+        </div>
+
+        {/* REsumes Card */}
+        {
+          resumes.length > 0 && (
+            <div className="resumes-section">
+              {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              )) }
+            </div>
+          )
+        }
+      </section>
+    </main>
   )
 };
